267fa87f824346e053051a000a5ccaa3f49cef79
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-07 12:01:44 -0400

    Create lakefile.lean

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «NonagonalCyclotomicResultants» where
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
+
+@[default_target]
+lean_lib «NonagonalCyclotomicResultants» where
+  srcDir := "."
